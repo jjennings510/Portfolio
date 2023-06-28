b5feafb9e2341cb9d26e1cea452a1817878f35cd
Jacob <jacobjennings510@gmail.com>
2023-06-28 15:17:38 -0700

    Highlight active link on navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,15 +7,47 @@ import { Component, HostListener } from '@angular/core';
 })
 export class NavbarComponent {
   isNavbarTransparent = true;
+  activeSection: string = '';
 
-  @HostListener('window:scroll')
+  @HostListener('window:scroll', ['$event'])
   onScroll(): void {
-    let scrollHeight = 600;
-
-    if (window.scrollY >= scrollHeight) {
+    let navbarHeightThreshold = 600;
+    if (window.scrollY >= navbarHeightThreshold) {
       this.isNavbarTransparent = false;
-    } else if (window.scrollY < scrollHeight) {
+    } else if (window.scrollY < navbarHeightThreshold) {
       this.isNavbarTransparent = true;
     }
+
+    this.highlightActiveLink();
+  }
+
+  highlightActiveLink() {
+    const sections = [
+      document.getElementById('home'),
+      document.getElementById('about'),
+      document.getElementById('skills'),
+      document.getElementById('experience'),
+      document.getElementById('portfolio'),
+      document.getElementById('contact'),
+    ];
+
+    const offset = 100;
+
+    const scrollPosition = window.scrollY;
+
+    let activeSection = "";
+
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i];
+      if (section) {
+        if (scrollPosition >= section?.offsetTop - offset) {
+          activeSection = section.id;
+        }
+      }
+    }
+
+    this.activeSection = activeSection;
+    console.log(scrollPosition)
+    console.log(activeSection);
   }
 }
